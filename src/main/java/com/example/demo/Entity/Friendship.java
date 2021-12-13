ca65fa6b669d50c6c19f2400e1af43f27412b4c0
pstepien-design <pstepien@interia.pl>
2021-12-13 17:39:02 +0100

    first commit

---
+++ src/main/java/com/example/demo/Entity/Friendship.java
@@ -0,0 +1,51 @@
+package com.example.demo.Entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "friendships", schema = "easyvote", uniqueConstraints={
+        @UniqueConstraint(columnNames = {"email1", "email2"})
+})
+public class Friendship {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email1;
+
+    @Column(nullable = false)
+    private String email2;
+
+    @Column()
+    private String status = "pending";
+
+    @Column()
+    private LocalDate dateEstablished;
+
+    public Friendship(String email1, String email2, LocalDate dateEstablished) {
+        this.email1 = email1;
+        this.email2 = email2;
+        this.dateEstablished = dateEstablished;
+    }
+
+}
